8f6d5c3056382d40d23bde1ec3b90c0189791a88
Maksym_enKO <ingool.ua@gmail.com>
2023-04-02 19:46:30 +0300

    volume (slider)

--- script.js
+++ script.js
@@ -60,6 +60,47 @@ function displayTime(time) {
   return `${minutes}:${seconds}`;
 }
 
+// Mute
+function toggleMute() {
+  volumeIcon.className = '';
+  if (video.volume) {
+    lastVolume = video.volume;
+    video.volume = 0;
+    volumeIcon.classList.add('fas', 'fa-volume-mute');
+    volumeIcon.setAttribute('title', 'Unmute');
+    volumeBar.style.width = 0;
+  } else {
+    video.volume = lastVolume;
+    volumeIcon.classList.add('fas', 'fa-volume-up');
+    volumeIcon.setAttribute('title', 'Mute');
+    volumeBar.style.width = `${lastVolume * 100}%`;
+  }
+}
+
+// Volume Bar
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth;
+  // Rounding volume up or down
+  if (volume < 0.1) {
+    volume = 0;
+  }
+  if (volume > 0.9) {
+    volume = 1;
+  }
+  volumeBar.style.width = `${volume * 100}%`;
+  video.volume = volume;
+  // Change icon depending on volume
+  volumeIcon.className = '';
+  if (volume > 0.7) {
+    volumeIcon.classList.add('fas', 'fa-volume-up');
+  } else if (volume < 0.7 && volume > 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-down');
+  } else if (volume === 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-off');
+  }
+  lastVolume = volume;
+}
+
 document.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
@@ -75,4 +116,6 @@ function init() {
   video.addEventListener('loadedmetadata', updateProgress);
   video.addEventListener('timeupdate', updateProgress);
   video.addEventListener('canplay', updateProgress);
+  volumeRange.addEventListener('click', changeVolume);
+  volumeIcon.addEventListener('click', toggleMute);
 }
